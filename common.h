cc8eb6d52413c5db35180da159860b3f92e79b40
yuki ozawa <yuki.ozawa.1985@gmail.com>
2020-07-27 03:34:16 +0900

    fix

--- common.h
+++ common.h
@@ -3,49 +3,15 @@
 #include <set>
 #include <array>
 #include <iostream>
-//
 #include <cstdio>
 #include <cstdint>
-
 //
-int32_t readI()
-{
-	int32_t v;
-	std::cin >> v;
-	return v;
-}
+using ll = long long;
+using namespace std;
+#define rep(i,n) for(int i=0;i<(n);++i)
+void yes(bool cond) { cond ? puts("Yes") : puts("No"); }
 //
-float readF()
+int main()
 {
-	float v;
-	std::cin >> v;
-	return v;
-}
-//
-std::vector<int32_t> readVecI(int32_t N)
-{
-	std::vector<int32_t> vec;
-	for (int32_t i = 0; i < N; ++i)
-	{
-		int32_t v;
-		std::cin >> v;
-		vec.push_back(v);
-	}
-	return vec;
-}
-//
-std::vector<float> readVecF(int32_t N)
-{
-	std::vector<float> vec;
-	for (int32_t i = 0; i < N; ++i)
-	{
-		float v;
-		std::cin >> v;
-		vec.push_back(v);
-	}
-	return vec;
-}
-//
-void main()
-{
+    cin.tie(0); ios::sync_with_stdio(false);
 }
