21712f41bf8374d0c8158759d97533684574d6f7
yuki ozawa <yuki.ozawa.1985@gmail.com>
2020-07-31 04:05:49 +0900

    update

--- common.h
+++ common.h
@@ -11,13 +11,13 @@
 //
 using ll = long long;
 using namespace std;
-#define rep(i,n) for(int i=0;i<(n);++i)
-void yes(bool cond) { cond ? puts("Yes") : puts("No"); }
+#define rep(i,n) for(ll i=0;i<(n);++i)
+inline void yes(bool cond) { cond ? puts("Yes") : puts("No"); }
 template<typename Type> inline void chmin(Type& a, Type b) { if (a > b) a = b; }
 template<typename Type> inline void chmax(Type& a, Type b) { if (a < b) a = b; }
-constexpr ll    BIGP = 1000000007;
-constexpr ll    INF_I = std::numeric_limits<ll>::max();
-constexpr float INF_F = std::numeric_limits<float>::infinity();
+#define BIGP   1000000007
+#define INF_I  std::numeric_limits<ll>::max()
+#define INF_F  std::numeric_limits<float>::infinity()
 //
 int main()
 {
