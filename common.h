adb72af9c483191c149a97cc0d19c9f0a9c2ae24
yuki ozawa <yuki.ozawa.1985@gmail.com>
2020-12-31 19:15:14 +0900

    update

--- common.h
+++ common.h
@@ -2,21 +2,15 @@
 #include <atcoder/all>
 #include <bits/stdc++.h>
 //
-using ll = long long;
 using namespace std;
 using namespace atcoder;
+using ll = long long;
 #define rep(i,n) for(ll i=0;i<(n);++i)
-#define rrep(i,n) for(ll i=(n)-1;0<=i;--i)
-#define outD(fpv) printf("%.10f", fpv)
 inline void Yes(bool cond) { cond ? puts("Yes") : puts("No"); }
-inline void YES(bool cond) { cond ? puts("YES") : puts("NO"); }
 template<typename Type> inline void chmin(Type& a, Type b) { if (a > b) a = b; }
 template<typename Type> inline void chmax(Type& a, Type b) { if (a < b) a = b; }
-constexpr ll INF_I = std::numeric_limits<ll>::max();
 constexpr ll BIGP = 1000000007L;
-constexpr double INF_F = std::numeric_limits<float>::infinity();
-constexpr double PI = 3.14159265358979323846L;
-using P = pair<ll, ll>;
+using P = tuple<ll, ll>;
 struct Init { Init() { cin.tie(0); ios::sync_with_stdio(false); } }init_;
 //
 ll solve()
